1e14fa6bdfce0606baadca27d5051262faeea689
AlexisAzael01 <153031687+AlexisAzael01@users.noreply.github.com>
2024-04-01 16:24:48 -0700

    agregúe botones cancel y save.

--- templates/Add_User.php
+++ templates/Add_User.php
@@ -27,55 +27,65 @@
                         <?php //?FORMULARIO AÑADIR USUARIO?>
                         <div class="add-user">
                             <form action="/agregar" method="">
-                                <div class="centrar-cochinero-de-alexis">
+                                <div class="form-container">
                                     <label for="name">Name: </label> <?php //? Nombre?> 
                                     <input type="text" id="name-input" class="name-input" name="name" placeholder="Type the name">
                                 </div>
-                                <div class="centrar-cochinero-de-alexis">
+                                <div class="form-container">
                                     <label for="name">Last Name: </label> <?php //? Apellido?>
                                     <input type="text" id="last-name-input" class="last-name-input" name="last-name" placeholder="Type the surname">
                                 </div>
-                                <div class="centrar-cochinero-de-alexis">
+                                <div class="form-container">
                                     <label for="name">Username: </label><?php //? Usuario?>
                                     <input type="text" id="username-input" class="username-input" name="username" placeholder="Type the username">
                                 </div>
-                                <div class="centrar-cochinero-de-alexis">
+                                <div class="form-container">
                                     <label for="name">Employee Id: </label> <?php //? Id de empleado?>
                                     <input type="text" id="employeeid-input" class="employeeid-input" name="employeeid" placeholder="Type the employee Id">
                                 </div>
-                                <div class="centrar-cochinero-de-alexis">
+                                <div class="form-container">
                                     <label for="name">Password: </label> <?php //? Contraseña?>
                                     <input type="password" id="name-input" class="name-input" name="name" placeholder="Type the password">
                                 </div>
-                                <div class="centrar-cochinero-de-alexis">
+                                <div class="form-container">
                                     <label for="name">Departament: </label> <?php //? Departamento?>
                                     <select name="departament" id="select-departament">
-                                </div>
-                                    <option value="">Select department</option>
-                                    <option value="">..</option>
-                                    <option value="">..</option>
-                                    <option value="">..</option>
-                                </select>
-                                <br>
-                                <label for="name">Subarea: </label> <?php //? subarea?>
-                                <select name="subarea" id="select-subarea">
-                                    <option value="">Select subarea</option>
-                                    <option value="">..</option>
-                                    <option value="">..</option>
-                                    <option value="">..</option>
-                                </select>
-                                <br>
-                                <label for="name">Privileges: </label> <?php //? Privilegios?>
-                                <select name="departament" id="select-departament">
-                                    <option value="">Select privileges</option>
-                                    <option value="">..</option>
-                                    <option value="">..</option>
-                                    <option value="">..</option>
-                                </select>
-                                <br>
-                                <label for="name">Phone: </label> <?php //? Contraseña?>
-                                <input type="text" id="phone-input" class="phone-input" name="phone" placeholder="Type the phone">
                                 
+                                    <option value="" disabled selected>Select department</option>
+                                    <option value="">..</option>
+                                    <option value="">..</option>
+                                    <option value="">..</option>
+                                    </select>
+                                </div>
+
+                                <div class="form-container">
+                                    <label for="name">Subarea: </label> <?php //? subarea?>
+                                    <select name="subarea" id="select-subarea">
+                                    <option value="" disabled selected>Select subarea</option>
+                                    <option value="">..</option>
+                                    <option value="">..</option>
+                                    <option value="">..</option>
+                                    </select>
+                                </div>
+                               
+                                <div class="form-container">
+                                    <label for="name">Privileges: </label> <?php //? Privilegios?>
+                                    <select name="departament" id="select-departament">
+                                    <option value="" disabled selected>Select privileges</option>
+                                    <option value="">..</option>
+                                    <option value="">..</option>
+                                    <option value="">..</option>
+                                    </select>
+                                </div>
+                                <div class="form-container">
+                                    <label for="name">Phone: </label> <?php //? Contraseña?>
+                                    <input type="text" id="phone-input" class="phone-input" name="phone" placeholder="Type the phone">
+                                </div>
+                                
+                                <div class="buttons-container">
+                                    <button class="cancel"><i class="fa-solid fa-x"></i>Cancel</button>
+                                    <button class="save"><i class="fa-regular fa-floppy-disk"></i>Save</button>
+                                </div>
                             </form>
                         </div>
                         
