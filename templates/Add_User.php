bafe1dcd16c5e0ff4c1ea749ccbbe7eae3381cba
Elcarlos2925 <cl154204@gmail.com>
2024-04-14 18:31:59 -0700

    add user update desing

--- templates/Add_User.php
+++ templates/Add_User.php
@@ -20,80 +20,92 @@
             <?php //? SIDEBAR?> 
             <?php echo "$sidebar";?>
             <?php //? CONTENEDOR PRINCIPAL?>
-                <div class="content-container"> 
-                    <div class="title-container">
-                        <h1>Add User</h1>
-                    </div>
-                    <div class="add-user-container">
-                        <h2>New User</h2>
-                        <?php //?FORMULARIO AÑADIR USUARIO?>
-                        <div class="add-user">
-                            <form action="/agregar" method="">
-                                <div class="form-container">
-                                    <label for="name">Name: </label> <?php //? Nombre?> 
-                                    <input type="text" id="name-input" class="name-input" name="name" placeholder="Type the name">
+            <div class="content-container"> 
+                <div class="title-container">
+                    <h1>Add User</h1>
+                </div>
+                <div class="add-user-container">
+                    <h2>New User</h2>
+                    <?php //?FORMULARIO AÑADIR USUARIO?>
+                    <div class="add-user">
+                        <form action="/agregar" method="" class="add-user-form">
+                            <div class="add-user">
+                                <div class="form-row">
+                                    <div class="form-group col-md-11">
+                                        <label for="name-input">Name: </label> <?php //? Nombre?> 
+                                        <input type="text" id="name-input" class="form-control" name="name" placeholder="Type the name">
+                                    </div>
                                 </div>
-                                <div class="form-container">
-                                    <label for="name">Last Name: </label> <?php //? Apellido?>
-                                    <input type="text" id="last-name-input" class="last-name-input" name="last-name" placeholder="Type the surname">
+                                <div class="form-row">
+                                    <div class="form-group col-md-11">
+                                        <label for="last-name-input">Last Name: </label> <?php //? Apellido?>
+                                        <input type="text" id="last-name-input" class="form-control" name="last-name" placeholder="Type the surname">
+                                    </div>
                                 </div>
-                                <div class="form-container">
-                                    <label for="name">Username: </label><?php //? Usuario?>
-                                    <input type="text" id="username-input" class="username-input" name="username" placeholder="Type the username">
+                                <div class="form-row">
+                                    <div class="form-group col-md-11">
+                                        <label for="username-input">Username: </label><?php //? Usuario?>
+                                        <input type="text" id="username-input" class="form-control" name="username" placeholder="Type the username">
+                                    </div>
                                 </div>
-                                <div class="form-container">
-                                    <label for="name">Employee Id: </label> <?php //? Id de empleado?>
-                                    <input type="text" id="employeeid-input" class="employeeid-input" name="employeeid" placeholder="Type the employee Id">
+                                <div class="form-row">
+                                    <div class="form-group col-md-11">
+                                        <label for="employeeid-input">Employee Id: </label> <?php //? Id de empleado?>
+                                        <input type="text" id="employeeid-input" class="form-control" name="employeeid" placeholder="Type the employee Id">
+                                    </div>
                                 </div>
-                                <div class="form-container">
-                                    <label for="name">Password: </label> <?php //? Contraseña?>
-                                    <input type="password" id="name-input" class="name-input" name="name" placeholder="Type the password">
+                                <div class="form-row">
+                                    <div class="form-group col-md-11">
+                                        <label for="password-input">Password: </label> <?php //? Contraseña?>
+                                        <input type="password" id="password-input" class="form-control" name="name" placeholder="Type the password">
+                                    </div>
                                 </div>
-                                <div class="form-container">
-                                    <label for="name">Departament: </label> <?php //? Departamento?>
-                                    <select name="departament" id="select-departament">
-                                
-                                    <option value="" disabled selected>Select department</option>
-                                    <option value="">..</option>
-                                    <option value="">..</option>
-                                    <option value="">..</option>
-                                    </select>
+                                <div class="form-row">
+                                    <div class="form-group col-md-5">
+                                        <label for="select-departament">Departament: </label> <?php //? Departamento?>
+                                        <select name="departament" id="select-departament" class="form-control">
+                                            <option value="" disabled selected>Select department</option>
+                                            <option value="">..</option>
+                                            <option value="">..</option>
+                                            <option value="">..</option>
+                                        </select>
+                                    </div>
+                                    <div class="form-group col-md-5">
+                                        <label for="select-subarea">Subarea: </label> <?php //? subarea?>
+                                        <select name="subarea" id="select-subarea" class="form-control">
+                                            <option value="" disabled selected>Select subarea</option>
+                                            <option value="">..</option>
+                                            <option value="">..</option>
+                                            <option value="">..</option>
+                                        </select>
+                                    </div>
+                                </div>
+                                <div class="form-row">
+                                    <div class="form-group col-md-5">
+                                        <label for="privileges-input">Privileges: </label> <?php //? Privilegios?>
+                                        <select name="departament" id="privileges-input" class="form-control">
+                                            <option value="" disabled selected>Select privileges</option>
+                                            <option value="">..</option>
+                                            <option value="">..</option>
+                                            <option value="">..</option>
+                                        </select>
+                                    </div>
+                                    <div class="form-group col-md-5">
+                                        <label for="phone-input">Phone: </label> <?php //? Contraseña?>
+                                        <input type="text" id="phone-input" class="form-control" name="phone" placeholder="Type the phone">
+                                    </div>
                                 </div>
 
-                                <div class="form-container">
-                                    <label for="name">Subarea: </label> <?php //? subarea?>
-                                    <select name="subarea" id="select-subarea">
-                                    <option value="" disabled selected>Select subarea</option>
-                                    <option value="">..</option>
-                                    <option value="">..</option>
-                                    <option value="">..</option>
-                                    </select>
+                                <div class="buttons-container move-rigth">
+                                    <button class="btn btn-danger"><i class="fa-solid fa-x"></i> Cancel</button>
+                                    <button class="btn btn-success"><i class="fa-regular fa-floppy-disk"></i> Save</button>
                                 </div>
-                               
-                                <div class="form-container">
-                                    <label for="name">Privileges: </label> <?php //? Privilegios?>
-                                    <select name="departament" id="select-departament">
-                                    <option value="" disabled selected>Select privileges</option>
-                                    <option value="">..</option>
-                                    <option value="">..</option>
-                                    <option value="">..</option>
-                                    </select>
-                                </div>
-                                <div class="form-container">
-                                    <label for="name">Phone: </label> <?php //? Contraseña?>
-                                    <input type="text" id="phone-input" class="phone-input" name="phone" placeholder="Type the phone">
-                                </div>
-                                
-                                <div class="buttons-container">
-                                    <button class="cancel"><i class="fa-solid fa-x"></i>Cancel</button>
-                                    <button class="save"><i class="fa-regular fa-floppy-disk"></i>Save</button>
-                                </div>
-                            </form>
-                        </div>
-                        
-
+                            </div>
+                        </form>
                     </div>
                 </div>
+            </div>
+
             <?php echo "$footer";?>
             <?php //? FOOTER?>
 
