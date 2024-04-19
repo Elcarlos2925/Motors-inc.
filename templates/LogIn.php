919387e5556dae72a0e189e6205657f5e7e6c769
Elcarlos2925 <cl154204@gmail.com>
2024-04-19 15:52:27 -0700

    variables css

--- templates/LogIn.php
+++ templates/LogIn.php
@@ -10,27 +10,25 @@ include '../home/icons.php';
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <?php echo ""; ?>
     <?php //? Cargar Css?>
-    <?php echo "$FontAwesome_css $Log_In_css $Bootstrap_4_6_0_css";?>
+    <?php echo "$FontAwesome_css $Style_css $Log_In_css $Bootstrap_4_6_0_css";?>
     <title>Log In</title>
 </head>
 <body>
-    <div class="main-container">
-        <form class="form-logIn">
-            <img id='logo' src='../static/assets/img/logo.jpeg' width='200px'>
-            <div class="form-group col-md-8 ">
-                <label for="">User name:</label>
-                <div class="form-row">
-                    <input type="text" placeholder="user name" class="form-control">
-                </div>                
-                <label for="">Password:</label>
-                <div class="form-row">
-                    <input type="password" placeholder="passwoord" class="form-control">
-                </div>
-                <div class="form-row">
-                    <button class="form-control btn-info">Log In</button>
-                </div>
+    <form class="form-logIn">
+        <img id='logo' src='../static/assets/img/logo.jpeg' width='200px'>
+        <div class="form-group col-md-8 ">
+            <label for="">User name:</label>
+            <div class="form-row">
+                <input type="text" placeholder="user name" class="form-control">
+            </div>                
+            <label for="">Password:</label>
+            <div class="form-row">
+                <input type="password" placeholder="passwoord" class="form-control">
             </div>
-        </form>
-    </div>
+            <div class="form-row">
+                <button class="form-control btn-info">Log In</button>
+            </div>
+        </div>
+    </form>
 </body>
 </html>
--- templates/SigIn.php
+++ templates/SigIn.php
@@ -8,41 +8,39 @@ include '../home/icons.php';
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <?php echo "$FontAwesome_css $Sig_In_css $Bootstrap_4_6_0_css";?>
+    <?php echo "$FontAwesome_css $Style_css $Sig_In_css $Bootstrap_4_6_0_css";?>
     <title>Sig In</title>
 </head>
 <body>
-    <div class="main-container">
-        <form action="" class="form-sigin">
-        <img id='logo' src='../static/assets/img/logo.jpeg' width='200px'>
-            <div class="form-group col-md-8">
-                
-                <div class="form-row">
-                    <label for="">Enter a user name</label>
-                    <input type="text" placeholder="user name" class="form-control">
-                </div>
-                <div class="form-row">
-                <label for="">Enter first Name(s)</label>
-                    <input type="text" placeholder="name(s)" class="form-control">
-                </div>
-                <div class="form-row">
-                    <label for="">Enter last Name</label>    
-                    <input type="text" placeholder="last name" class="form-control">
-                </div>
-                
-                <div class="form-row">
-                    <label for="">Create a password</label>
-                    <input type="password" placeholder="password" class="form-control">
-                </div>
-                <div class="form-row">
-                    <label for="">Confirm password</label>
-                    <input type="password" placeholder="confirm password" class="form-control">
-                </div>
-                <div class="form-row">
-                    <button class="form-control  btn-info">Sig In</button>
-                </div>
+    <form action="" class="form-sigin">
+    <img id='logo' src='../static/assets/img/logo.jpeg' width='200px'>
+        <div class="form-group col-md-8">
+            
+            <div class="form-row">
+                <label for="">Enter a user name</label>
+                <input type="text" placeholder="user name" class="form-control">
             </div>
-        </form>
-    </div>
+            <div class="form-row">
+            <label for="">Enter first Name(s)</label>
+                <input type="text" placeholder="name(s)" class="form-control">
+            </div>
+            <div class="form-row">
+                <label for="">Enter last Name</label>    
+                <input type="text" placeholder="last name" class="form-control">
+            </div>
+            
+            <div class="form-row">
+                <label for="">Create a password</label>
+                <input type="password" placeholder="password" class="form-control">
+            </div>
+            <div class="form-row">
+                <label for="">Confirm password</label>
+                <input type="password" placeholder="confirm password" class="form-control">
+            </div>
+            <div class="form-row">
+                <button class="form-control  btn-info">Sig In</button>
+            </div>
+        </div>
+    </form>
 </body>
 </html>
