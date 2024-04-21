d5eabbdc0e1ef6c235496b575b960b0f519ff337
Elcarlos2925 <cl154204@gmail.com>
2024-04-21 12:40:42 -0700

    correction to title

--- templates/Pass-Rate.php
+++ templates/Pass-Rate.php
@@ -11,7 +11,7 @@
     <?php echo "$Pass_Rate_icon"; ?>
     <?php //? Cargar Css?>
     <?php echo "$FontAwesome_css $Style_css $Pass_Rate_css $Bootstrap_4_6_0_css $Bootstrap_4_6_0_css $jQuery_js $ClockPicker_js $ClockPicker_css";?>
-    <title>Pass Rate ITC</title>
+    <title>Pass Rate ICT</title>
 </head>
 <body>
 
@@ -25,7 +25,7 @@
 
         <div class="content-container">
             <div class="title-container">
-                <h1>Pass Rate ITC</h1>
+                <h1>Pass Rate ICT</h1>
             </div>
             <div class="pass-rate-container">
                 <?php //?Options ?>
